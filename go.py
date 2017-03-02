96e5258628f1108ed545d4a0cac64a82b7fd6b86
kamushekp <kamushekp@mail.ru>
2017-03-02 13:23:21 +0700

    Update go.py

--- go.py
+++ go.py
@@ -1,60 +1,67 @@
 
-kern='epanech'
+
 p=1
-h = 2.1
+h = 3.0
 purpose=[1,2,3]
 x0=[0.8,1.2,2.2,1.8,2.8,3.2]
 from find_design import find_best_design as fbd
-resNM,resDIF=fbd(purpose,x0,p,h,kern)
+
 
 
 from M import M
 import numpy as np
-
-print('kernel = {0}, p = {1}, h = {2}\n'.format(kern,p,h))
-
-print('\n\nNelder-Mead')
-
-res=0
-for i in range(len(purpose)):
-    a=M(purpose[i],resNM.x,kern,p,h)
-    print(np.linalg.det(a))
-    res+=np.linalg.det(a)
-
-print(res)
-print(list(sorted(resNM.x)))
-
-print('\n\nDiff. evolution')
-res=0
-for i in range(len(purpose)):
-    a=M(purpose[i],resDIF.x,kern,p,h)
-    print(np.linalg.det(a))
-    res+=np.linalg.det(a)
-
-print(res)
-print(list(sorted(resDIF.x)))
-
-'''
+def main():
+    print('kernel = {0}, p = {1}, h = {2}\n'.format(kern,p,h))
+    resNM,resDIF=fbd(purpose,x0,p,h,kern)
+    print('\n\nNelder-Mead')
+    
+    res=0
+    for i in range(len(purpose)):
+        a=M(purpose[i],resNM.x,kern,p,h)
+        print(np.linalg.det(a))
+        res+=np.linalg.det(a)
+    
+    print(res)
+    print(list(sorted(resNM.x)))
+    
+    print('\n\nDiff. evolution')
+    res=0
+    for i in range(len(purpose)):
+        a=M(purpose[i],resDIF.x,kern,p,h)
+        print(np.linalg.det(a))
+        res+=np.linalg.det(a)
+    
+    print(res)
+    print(list(sorted(resDIF.x)))
 
 
-def modif_M(point,design):
-    fish=M(point,design,kern,p,h)
-    M12=fish[:1,1:]
-    M22inv=np.linalg.inv(fish[1:,1:])
-    temp=np.dot(M12,M22inv)
-    temp=np.dot(temp,M12.T)
-    res=float(fish[:1,:1]-temp)
-    return res
+def get_graphics_of_parts():
+    X_ = []
+    Y_epan = []
+    Y_gauss =  []  
+    
+    for step in np.linspace(0.4, 5, 25):
+        kern='gauss'
+        print(step)
+        resNM, resDIF=fbd(purpose,x0,p,step,kern)
+        dets = [np.linalg.det(M(purpose[i],resNM.x,kern,p,step)) for i in range(3)]
+        Y_epan.append(max(dets) / sum(dets))
         
-des=[-0.00937618,  0.00937618,  0.3987743 ,  0.6012257]
-result=modif_M(0,des)+modif_M(0.5,des)
+    for step in np.linspace(0.4, 5, 25):
+        kern='epanech'
+        print(step)
+        resNM, resDIF=fbd(purpose,x0,p,step,kern)
+        dets = [np.linalg.det(M(purpose[i],resDIF.x,kern,p,step)) for i in range(3)]
+        Y_gauss.append(max(dets) / sum(dets))
+        X_.append(step)
+    print('\n\nGauss - {0}\n\nEpanech - {1}'.format(Y_gauss, Y_epan))
 
 
-print('\n\nDiff. evolution')
-a=M(1,resDIF.x,kern,p,h)
-b=M(2,resDIF.x,kern,p,h)
-print(np.linalg.det(a)+np.linalg.det(b))
-print('first {}'.format(np.linalg.det(a)))
-print('second {}'.format(np.linalg.det(b)))
-print(list(resDIF.x))
-'''
+    import matplotlib.pyplot as plt
+    
+    plt.plot(X_, Y_epan, X_, Y_gauss)
+    plt.show()
+get_graphics_of_parts()
+
+
+
